88a2e647d56da8b3a52085779f1c09e466aa823f
ShayanShahmohammadi <shahmohammadish66@gmail.com>
2025-01-16 09:32:22 +0330

    Tree in progress

--- Tree.h
+++ Tree.h
@@ -1,8 +1,21 @@
-//
-// Created by shayan on 1/16/25.
-//
+#include <stdlib.h>
+
+#include "Node.h"
 
 #ifndef DSA_PROJECT_TREE_H
 #define DSA_PROJECT_TREE_H
+
+Node * NIL;
+
+void trance_plant(Node *, Node *);
+void left_rotation(Node *);
+void right_rotation(Node *);
+
+void insert_new_node(Node *, Node *);
+void insertion_fixup();
+
+Node * build_tree(Table *);
+
+Element * search_by_student_number(Table *, unsigned int);
 
 #endif //DSA_PROJECT_TREE_H
