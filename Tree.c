adb620318a0a87a70218cac9b9e38a17669d1330
ShayanShahmohammadi <shahmohammadish66@gmail.com>
2025-01-16 08:25:47 +0330

    File Tree created

---
+++ Tree.c
@@ -0,0 +1,5 @@
+//
+// Created by shayan on 1/16/25.
+//
+
+#include "Tree.h"
---
+++ Tree.h
@@ -0,0 +1,8 @@
+//
+// Created by shayan on 1/16/25.
+//
+
+#ifndef DSA_PROJECT_TREE_H
+#define DSA_PROJECT_TREE_H
+
+#endif //DSA_PROJECT_TREE_H
