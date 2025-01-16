3ed2a97cd6f820aa72d788acc1873cf84b3eee22
ShayanShahmohammadi <shahmohammadish66@gmail.com>
2025-01-16 08:24:54 +0330

    Node file finished

--- Node.c
+++ Node.c
@@ -1,5 +1,44 @@
-//
-// Created by shayan on 1/14/25.
-//
+#include <stdlib.h>
+#include <stdbool.h>
 
 #include "Node.h"
+
+Node * node_constructor(Element * element) {
+    Node * new_node = (Node *) calloc(1, sizeof(Node));
+
+    new_node->record = element->record;
+    new_node->is_black = true;
+    new_node->parent = NULL;
+    new_node->left_child = NULL;
+    new_node->right_child = NULL;
+
+    return new_node;
+}
+
+void toggle_color(Node * node) {
+    node->is_black = !node->is_black;
+}
+
+void change_to_this_color(Node * node, bool new_color) {
+    node->is_black = new_color;
+}
+
+void put_as_parent(Node * base_node, Node * relative_node, bool is_left_child) {
+    base_node->parent = relative_node;
+    if (is_left_child) {
+        relative_node->left_child = base_node;
+    }
+    else {
+        relative_node->right_child = base_node;
+    }
+}
+
+void put_as_left_child(Node * base_node, Node * relative_node) {
+    base_node->left_child = relative_node;
+    relative_node->parent = base_node;
+}
+
+void put_as_right_child(Node * base_node, Node * relative_node) {
+    base_node->right_child = relative_node;
+    relative_node->parent = base_node;
+}
--- Node.h
+++ Node.h
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 
 #include "Record.h"
+#include "Table.h"
 
 #ifndef DSA_PROJECT_NODE_H
 #define DSA_PROJECT_NODE_H
@@ -13,5 +14,14 @@ struct template_6 {
     struct template_6 * right_child;
 };
 typedef struct template_6 Node;
+
+Node * node_constructor(Element *);
+
+void toggle_color(Node *);
+void change_to_this_color(Node *, bool);
+
+void put_as_parent(Node *, Node *, bool);
+void put_as_left_child(Node *, Node *);
+void put_as_right_child(Node *, Node *);
 
 #endif //DSA_PROJECT_NODE_H
